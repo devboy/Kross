9a5cac29940e8541bedac8f084c1208b6e2c3752
devboy <dominic.graefen@gmail.com>
2015-11-21 13:19:29 +0100

    Adds library reference to App.OSX and displays window-title

--- src/Kross.App.OSX/MainWindow.fs
+++ src/Kross.App.OSX/MainWindow.fs
@@ -16,3 +16,4 @@ type MainWindow =
 
     override x.AwakeFromNib () =
         base.AwakeFromNib ()
+        x.Title <- Kross.Library.Name
--- src/Kross/Library.fs
+++ src/Kross/Library.fs
@@ -1,16 +1,7 @@
 namespace Kross
 
-/// Documentation for my library
-///
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
 module Library = 
-  
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+
+    let Name = "Kross"
+
+    let hello num = 42
