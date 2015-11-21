a97908f5ab2b6afbdbe73a42ebd6c3ad3cff2967
devboy <dominic.graefen@gmail.com>
2015-11-21 13:04:38 +0100

    Adds Kross.App.OSX project

---
+++ src/Kross.App.OSX/Main.fs
@@ -0,0 +1,10 @@
+﻿namespace Kross.App.OSX
+open System
+open AppKit
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        NSApplication.Init ()
+        NSApplication.Main (args)
+        0
